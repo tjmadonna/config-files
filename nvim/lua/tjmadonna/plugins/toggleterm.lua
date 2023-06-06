6dba8f62fa0aafbc6e74336020d8087af6136b82
Tyler Madonna <tyler@tylermadonna.com>
2023-06-06 19:38:27 -0400

    Removed keymaps for inside toggleterm

--- nvim/lua/tjmadonna/plugins/toggleterm.lua
+++ nvim/lua/tjmadonna/plugins/toggleterm.lua
@@ -10,13 +10,6 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "kj", [[<C-\><C-n>]], opts)
-	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-	vim.keymap.set("t", "<C-d>", [[<Cmd>wincmd c<CR>]], opts)
 	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
