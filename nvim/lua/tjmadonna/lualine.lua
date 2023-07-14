9992e744c2712d0d833b10439ce88417da6ef8ab
Tyler Madonna <tjm159@pitt.edu>
2023-07-14 13:24:57 -0400

    Code cleanup

--- nvim/lua/tjmadonna/lualine.lua
+++ nvim/lua/tjmadonna/lualine.lua
@@ -1,7 +1,7 @@
-require('lualine').setup({
+require("lualine").setup({
   options = {
     theme = "gruvbox_dark",
-    component_separators = { left = '|', right = '|'},
-    section_separators = { left = ' ', right = ' '},
+    component_separators = { left = "|", right = "|"},
+    section_separators = { left = " ", right = " "},
   },
 })
--- nvim/lua/tjmadonna/toggleterm.lua
+++ nvim/lua/tjmadonna/toggleterm.lua
@@ -14,7 +14,7 @@ toggleterm.setup({
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
 	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
