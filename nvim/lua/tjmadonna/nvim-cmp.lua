2be48c6ca73633a83acf7848700cd5427ba05404
Tyler Madonna <tyler@tylermadonna.com>
2023-11-06 18:30:34 -0500

    Added scroll docs mapping to completion

--- nvim/lua/tjmadonna/nvim-cmp.lua
+++ nvim/lua/tjmadonna/nvim-cmp.lua
@@ -4,34 +4,6 @@ local luasnip = require("luasnip")
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-}
-
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -44,8 +16,8 @@ cmp.setup({
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
-		-- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		-- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-e>"] = cmp.mapping({
 			i = function()
@@ -64,12 +36,11 @@ cmp.setup({
 			end,
 		}),
 	},
+
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
-			-- Kind icons
 			vim_item.kind = string.format("%s", vim_item.kind)
-			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
 				luasnip = "[Snippet]",
