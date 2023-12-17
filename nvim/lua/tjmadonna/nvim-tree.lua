2184d7b0b40b36cd2c77075b2a90b1e29c2e9b16
Tyler Madonna <tyler@tylermadonna.com>
2023-12-17 11:32:33 -0500

    Changed nvim-tree to floating window

--- nvim/lua/tjmadonna/nvim-tree.lua
+++ nvim/lua/tjmadonna/nvim-tree.lua
@@ -25,6 +25,9 @@ local function on_attach(bufnr)
 	vim.keymap.set("n", "<C-c>", api.tree.close, opts("Close"))
 end
 
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
+
 require("nvim-tree").setup({
 	on_attach = on_attach,
 	sort_by = "case_sensitive",
@@ -35,7 +38,30 @@ require("nvim-tree").setup({
 	},
 	view = {
 		signcolumn = "no",
-		width = "15%",
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
 	},
 	renderer = {
 		root_folder_label = title,
