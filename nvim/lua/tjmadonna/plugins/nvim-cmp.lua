4a81504cfe1915278951d8e9259fc99e9999fe4e
Tyler Madonna <tyler@tylermadonna.com>
2024-02-18 17:18:16 -0500

    Removed lskind from nvim

--- nvim/lua/tjmadonna/plugins/nvim-cmp.lua
+++ nvim/lua/tjmadonna/plugins/nvim-cmp.lua
@@ -7,14 +7,11 @@ return {
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
   },
   config = function()
     local cmp = require("cmp")
 
     local luasnip = require("luasnip")
-
-    local lspkind = require("lspkind")
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
@@ -44,13 +41,6 @@ return {
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
     })
   end,
 }
