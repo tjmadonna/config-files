d3722ea89c2715ae85976f816036449e6c8e7b6f
Tyler Madonna <tyler@tylermadonna.com>
2024-02-18 19:59:23 -0500

    Added python docstring snippet

--- nvim/lua/tjmadonna/plugins/nvim-cmp.lua
+++ nvim/lua/tjmadonna/plugins/nvim-cmp.lua
@@ -15,6 +15,7 @@ return {
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").load({paths = vim.fn.stdpath("config") .. "/lua/tjmadonna/snippets" })
 
     cmp.setup({
       completion = {
---
+++ nvim/lua/tjmadonna/snippets/python.lua
@@ -0,0 +1,9 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s("docnumpy", {
+    t({ '"""', "", "Parameters", "----------", "", "Returns", "-------", "", '"""' }),
+  }),
+}
