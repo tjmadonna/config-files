dba6fa86bf2580c4c1a5774b2f708173101a27ff
Tyler Madonna <tyler@tylermadonna.com>
2024-04-13 22:09:55 -0400

    Changed lualine status to match the colorscheme

--- nvim/lua/tjmadonna/plugins/lualine.lua
+++ nvim/lua/tjmadonna/plugins/lualine.lua
@@ -5,6 +5,18 @@ return {
   },
   config = function()
     local lualine = require("lualine")
+    local colors = {
+      black = "#282828",
+      white = "#ebdbb2",
+      red = "#fb4934",
+      green = "#b8bb26",
+      blue = "#83a598",
+      yellow = "#fe8019",
+      gray = "#a89984",
+      darkgray = "#3c3836",
+      lightgray = "#504945",
+      inactivegray = "#7c6f64",
+    }
 
     lualine.setup({
       options = {
@@ -18,7 +30,33 @@ return {
             "buffers",
             icons_enabled = false,
             mode = 2,
-            show_modified_status = true,
+            use_mode_colors = false,
+            buffers_color = {
+              active = function()
+                local mode_names = {
+                  n = "lualine_a_normal",
+                  i = "lualine_a_insert",
+                  v = "lualine_a_visual",
+                  V = "lualine_a_visual",
+                  c = "lualine_a_command",
+                  R = "lualine_a_replace",
+                }
+                local mode = vim.api.nvim_get_mode().mode
+                return mode_names[mode]
+              end,
+              inactive = function()
+                local mode_names = {
+                  n = { bg = colors.darkgray, fg = colors.white },
+                  i = { bg = colors.lightgray, fg = colors.white },
+                  v = { bg = colors.inactivegray, fg = colors.black },
+                  V = { bg = colors.inactivegray, fg = colors.black },
+                  c = { bg = colors.inactivegray, fg = colors.black },
+                  R = { bg = colors.darkgray, fg = colors.white },
+                }
+                local mode = vim.api.nvim_get_mode().mode
+                return mode_names[mode]
+              end,
+            },
             hide_filename_extension = false,
             show_filename_only = true,
             symbols = {
