e1b9dd0d3946d164d918d1220518a4c26e2999db
Tyler Madonna <tyler@tylermadonna.com>
2024-04-15 15:16:51 -0400

    Commented out ecma injections for now

--- nvim/after/queries/ecma/injections.scm
+++ nvim/after/queries/ecma/injections.scm
@@ -1,15 +1,15 @@
 ; extends
 
 ; matches "await sql<MyModel>`...`" for postgres library
-(call_expression
-  function: (await_expression
-    (identifier) @injection.language (#eq? @injection.language "sql")
-  )
-  type_arguments: (type_arguments)
-  arguments: [
-    (arguments
-      (template_string) @injection.content)
-    (template_string) @injection.content
-  ]
-  (#set! injection.include-children)
-)
+; (call_expression
+;   function: (await_expression
+;     (identifier) @injection.language (#eq? @injection.language "sql")
+;   )
+;   type_arguments: (type_arguments)
+;   arguments: [
+;     (arguments
+;       (template_string) @injection.content)
+;     (template_string) @injection.content
+;   ]
+;   (#set! injection.include-children)
+; )
