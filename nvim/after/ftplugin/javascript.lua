a09a559e358d167df63227eabda775361aa30db3
Tyler Madonna <tyler@tylermadonna.com>
2024-04-15 10:52:32 -0400

    Set the javascript tabstop and shiftwidth

--- nvim/after/ftplugin/javascript.lua
+++ nvim/after/ftplugin/javascript.lua
@@ -1,3 +1,5 @@
 local opt = vim.opt
-opt.colorcolumn = "80"
+opt.colorcolumn = "100"
 opt.formatoptions:remove({ "c", "r", "o" }) -- Stop newline continution of comments
+opt.tabstop = 4
+opt.shiftwidth = 4
