403d3992386478520346fea93345b0e43b11ba38
Tyler Madonna <tjm159@pitt.edu>
2024-07-20 15:23:10 -0400

    Changed nvim-tree to ignore files in gitignore

--- nvim/lua/tjmadonna/lazy.lua
+++ nvim/lua/tjmadonna/lazy.lua
@@ -1,25 +1,32 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "tjmadonna.plugins" }, { import = "tjmadonna.plugins.lsp" } }, {
-	install = {
-		colorscheme = { "catppuccin" },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
+  install = {
+    colorscheme = { "catppuccin" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+  profiling = {
+    -- Enables extra stats on the debug tab related to the loader cache.
+    -- Additionally gathers stats about all package.loaders
+    loader = true,
+    -- Track each new require in the Lazy profiling tab
+    require = false,
+  },
 })
