17428058b1ccf4217563c83e03c7f3cdb30be924
Tyler Madonna <tyler@tylermadonna.com>
2024-07-20 16:16:47 -0400

    Disabled lazy profiling by default

--- nvim/lua/tjmadonna/lazy.lua
+++ nvim/lua/tjmadonna/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({ { import = "tjmadonna.plugins" }, { import = "tjmadonna.
   profiling = {
     -- Enables extra stats on the debug tab related to the loader cache.
     -- Additionally gathers stats about all package.loaders
-    loader = true,
+    loader = false,
     -- Track each new require in the Lazy profiling tab
     require = false,
   },
