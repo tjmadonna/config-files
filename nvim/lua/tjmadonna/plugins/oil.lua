daa29a79bcf5c752b32d52745f8cc13f7c16da1f
Tyler Madonna <tjm159@pitt.edu>
2024-08-19 13:56:52 -0400

    Added show hidden files in oil config

--- nvim/lua/tjmadonna/plugins/oil.lua
+++ nvim/lua/tjmadonna/plugins/oil.lua
@@ -3,6 +3,10 @@ vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open cwd in oil" })
 return {
   "stevearc/oil.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
