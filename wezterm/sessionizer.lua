d9912bad7e7e19339cfc4d2d4f2251ec9cda8b58
Tyler Madonna <tjm159@pitt.edu>
2024-08-20 14:53:02 -0400

    Added directories to exclude in sessionizer

--- wezterm/sessionizer.lua
+++ wezterm/sessionizer.lua
@@ -4,19 +4,28 @@ local act = wezterm.action
 local M = {}
 
 local fd = "/opt/homebrew/bin/fd"
-local rootPath = os.getenv("HOME") .. "/Development/"
+local root_path = os.getenv("HOME") .. "/Development/"
+local exclude_dirs = {}
 
 M.toggle = function(window, pane)
   local projects = {}
 
-  local success, stdout, stderr = wezterm.run_child_process({
+  local fd_args = {
     fd,
     "-HI",
     "-td",
     "^.git$",
     "--max-depth=3",
-    rootPath,
-  })
+  }
+
+  for _, dir in ipairs(exclude_dirs) do
+    table.insert(fd_args, "--exclude")
+    table.insert(fd_args, dir)
+  end
+
+  table.insert(fd_args, root_path)
+
+  local success, stdout, stderr = wezterm.run_child_process(fd_args)
 
   if not success then
     wezterm.log_error("Failed to run fd: " .. stderr)
