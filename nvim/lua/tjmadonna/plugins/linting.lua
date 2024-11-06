2e6fd02d3797a00e813cc710f1a8b246b7440297
Tyler Madonna <tyler@tylermadonna.com>
2024-11-06 17:38:29 -0500

    Added filetypes to enable linting

--- nvim/lua/tjmadonna/plugins/linting.lua
+++ nvim/lua/tjmadonna/plugins/linting.lua
@@ -1,6 +1,7 @@
 return {
   "mfussenegger/nvim-lint",
   event = { "BufReadPre", "BufNewFile" },
+  ft = "javascript,typescript,typescriptreact,javascriptreact,go,python",
   config = function()
     local lint = require("lint")
 
