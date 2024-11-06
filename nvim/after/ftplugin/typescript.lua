65ff9a3fe2fd7f58c23ca3be9eb2b12def16ce6f
Tyler Madonna <tyler@tylermadonna.com>
2024-11-06 17:40:31 -0500

    Updated go, js, ts, and svelte ftplugins

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -1,3 +1,5 @@
 local opt = vim.opt
-opt.colorcolumn = "80"
+opt.colorcolumn = "120"
 opt.formatoptions:remove({ "c", "r", "o" }) -- Stop newline continution of comments
+opt.tabstop = 4
+opt.shiftwidth = 4
